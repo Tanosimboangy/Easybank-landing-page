2e7aa78cd02c4dda6e29d6fd5b6e03589adc9f21
Tanosimboangy <jacquit.aja@onja.org>
2021-01-16 09:34:13 +0300

    creating all neccessary files such as index.html, index.scss, index.js

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <h2>I am the footer !</h2>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <>
+            <header>I am the header of main</header>
+            <section
+            className="first_section"></section>
+            <section className="second_section"></section>
+        </>
+    )
+}
+
+export default Main

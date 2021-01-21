5cfcc6c478d1a1bf5c63f9c42472410d1b163d0b
Tanosimboangy <jacquit.aja@onja.org>
2021-01-21 14:27:46 +0300

    adding some fixation

--- components/Second_section.js
+++ components/Second_section.js
@@ -18,19 +18,19 @@ function Seecond_section() {
                         <p>The world is getting smaller and we're becoming more mobile. So why should you be forced to only receive in a single...</p>
                     </article>
                     <article>
-                    <img src={imgRestaurant}/>
+                        <img src={imgRestaurant}/>
                         <small>By Wilson Huntton</small>
                         <h5>Treat yourself without worrying about money</h5>
                         <p>Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you ...</p>
                     </article>
                     <article>
-                    <img src={imgPlane}/>
+                        <img src={imgPlane}/>
                         <small>By Wilson Huntton</small>
                         <h5>Take your Easybank card wherever you go</h5>
                         <p>We want you to enjoy your travels. This is why we don't charge any fees on purchases while you're abroad. We'll even show you ...</p>
                     </article>
                     <article>
-                    <img src={imgConfetti}/>
+                        <img src={imgConfetti}/>
                         <small>By Claire Robinson</small>
                         <h5>Our invite-only Beta accounts are now live!</h5>
                         <p>After a lot of hard work by the whole team, we're excited to launch our closed beta. It's easy to request an invite through the site ...</p>
